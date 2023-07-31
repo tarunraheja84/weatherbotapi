f989186491466ed968f620dcaca3ad72632eec84
Tarun <tarunraheja84@gmail.com>
2023-08-01 01:40:23 +0530

    next commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:true});
   app.useGlobalPipes(new ValidationPipe({whitelist:true}));
   await app.listen(process.env.PORT, '0.0.0.0');
 }
