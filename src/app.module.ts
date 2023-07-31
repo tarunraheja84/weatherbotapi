0e180df0a874b237241b8640e4a5a262998287e5
Tarun <tarunraheja84@gmail.com>
2023-08-01 02:23:24 +0530

    next commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-    imports: [UserModule, AuthModule, TelegramModule],
+    imports: [UserModule, AuthModule],
 })
 export class AppModule{}
