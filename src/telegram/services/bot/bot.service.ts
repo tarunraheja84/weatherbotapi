e4cd02439d39de06e1e074a9c48b9df68e472c36
Tarun <tarunraheja84@gmail.com>
2023-07-31 22:35:56 +0530

    next commit

--- src/telegram/services/bot/bot.service.ts
+++ src/telegram/services/bot/bot.service.ts
@@ -3,20 +3,12 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // Telegram bot token
 const TELEGRAM_TOKEN = "6054618964:AAHpgM7LJmM4ZQEQupMNSRDTfkCCod_u2Mg";
-const WEBHOOK_URL = "https://weatherbotapi.vercel.app";
 
 @Injectable()
 export class BotService {
     public readonly bot: any;
     constructor(){
-        this.bot = new TelegramBot(TELEGRAM_TOKEN);
-        this.bot.setWebHook(WEBHOOK_URL)
-        .then(() => {
-            console.log('Webhook set up successfully!');
-        })
-        .catch((error) => {
-            console.error('Error setting up webhook:', error);
-        });
+        this.bot = new TelegramBot(TELEGRAM_TOKEN, {polling:true});
     }
 
     getBotInstance(): any {
